3398d0a7af79b4d644ac22c1056465313d80da57
vladdlab <vladbvv2000@gmail.com>
2020-09-30 12:36:41 +0300

    feat: start project

---
+++ api/server/src/config/config.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+
+module.exports = {
+
+  development: {
+    database: 'onlinestore',
+    username: 'postgres',
+    password: '30093009',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+
+  test: {
+    database: 'onlinestore_test',
+    username: 'postgres',
+    password: '30093009',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+
+  production: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  }
+};
